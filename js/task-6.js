e34c19867a17b29691dd2907ca6a5e9e7b2a45db
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2024-08-21 12:39:41 +0300

    Added 6 task

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input'),
+  create = document.querySelector('[data-create]'),
+  destroy = document.querySelector('[data-destroy]'),
+  boxes = document.querySelector('#boxes');
+  
+
+function createBoxes(amount) {
+  let size = 30;
+  let box = '';
+
+  for (let i = 0; i < amount; i++) {
+    let color = getRandomHexColor();
+    box += `<div class="new" style="width:${size}px; height:${size}px; background-color:${color};"></div>`;
+    size += 10;
+  };
+  boxes.innerHTML = box;
+};
+
+function handleCreate() {
+    const amount = parseInt(input.value, 10);
+    if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
+      alert('Please enter a number between 1 and 100.');
+  }
+}
+function handleDestroy() {
+  boxes.innerHTML = '';
+}
+create.addEventListener('click', handleCreate);
+destroy.addEventListener('click', handleDestroy);
+
+
+
+
