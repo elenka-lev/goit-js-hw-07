625588a11fd9a9d727e398b940a6b10beb693666
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2024-08-21 10:22:09 +0300

    Added 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+function handleColor(event) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spanColor.textContent = color;
+};
+
+btn.addEventListener('click', handleColor);
